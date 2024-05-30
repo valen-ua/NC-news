b7b42dd56517b1f73d369069a946d41d41f49264
Valentyna Borysenko <valentynaborysenko@Valentynas-MacBook-Pro-2.local>
2024-05-30 15:36:15 +0100

    Completed task 7

--- models/comments.models.js
+++ models/comments.models.js
@@ -13,3 +13,21 @@ exports.selectCommentsByArticleId = (article_id) => {
         }
     })
 }
+
+exports.insertComment = (article_id, author, body) => {
+    if(!author || !body) {return Promise.reject ({status: 400, msg: "Bad Request"})}
+    return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
+    .then(({rows}) => {
+        if(rows.length === 0) {return Promise.reject({status: 404, msg: "Not Found"})}
+        else {return db
+            .query('INSERT INTO comments (author, body, article_id) VALUES ($1, $2, $3) RETURNING author, body;', [author, body, article_id])
+            .then(({rows}) => {
+                const comment = rows[0]
+                return comment
+            })}
+    })
+    }
+    
+    
+
+   
